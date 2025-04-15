0326847b83b48f469f2bee4df7ddd92be5b2cda8
Lubnin_Ilia <wolframalpha7134@gmail.com>
2025-04-15 19:59:07 +0300

    Setup recycler view

--- DeezerApp/feature-found-tracks/src/main/java/android/learn/found/presentation/FoundTracksFragment.kt
+++ DeezerApp/feature-found-tracks/src/main/java/android/learn/found/presentation/FoundTracksFragment.kt
@@ -1,35 +1,43 @@
 package android.learn.found.presentation
 
+import android.learn.found.databinding.FragmentFoundTracksBinding
+import android.learn.list.presentation.adapter.TracksAdapter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.learn.found.R
+import androidx.fragment.app.Fragment
 
 class FoundTracksFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
+    private val binding by lazy {
+        FragmentFoundTracksBinding.inflate(layoutInflater)
+    }
+
+    private val adapter by lazy {
+        TracksAdapter()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_found_tracks, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+
+    }
+
+    private fun setupRecyclerView() {
+        binding.tracksRv.adapter = adapter
     }
 
     companion object {
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             FoundTracksFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
             }
     }
 }
