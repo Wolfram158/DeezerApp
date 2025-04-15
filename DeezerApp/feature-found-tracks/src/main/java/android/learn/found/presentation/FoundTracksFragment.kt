af7be110ff0a20745865444d6d184d7e49512b8d
Lubnin_Ilia <wolframalpha7134@gmail.com>
2025-04-15 21:37:14 +0300

    Continue developing FoundTracksFragment

--- DeezerApp/feature-found-tracks/src/main/java/android/learn/found/presentation/FoundTracksFragment.kt
+++ DeezerApp/feature-found-tracks/src/main/java/android/learn/found/presentation/FoundTracksFragment.kt
@@ -1,17 +1,30 @@
 package android.learn.found.presentation
 
+import android.content.Context
 import android.learn.found.databinding.FragmentFoundTracksBinding
+import android.learn.list.presentation.ViewModelFactory
 import android.learn.list.presentation.adapter.TracksAdapter
+import android.learn.utils.Error
+import android.learn.utils.Progress
+import android.learn.utils.Result
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class FoundTracksFragment : Fragment() {
     private val binding by lazy {
         FragmentFoundTracksBinding.inflate(layoutInflater)
     }
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
 
     private val adapter by lazy {
         TracksAdapter(onGotoTrackListener = object: TracksAdapter.OnGotoTrackListener {
@@ -19,6 +32,20 @@ class FoundTracksFragment : Fragment() {
                 TODO("Not yet implemented")
             }
         })
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[FoundTracksViewModel::class.java]
+    }
+
+    private val component by lazy {
+        (requireActivity().application as App).component
+    }
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+
+        super.onAttach(context)
     }
 
     override fun onCreateView(
@@ -32,11 +59,23 @@ class FoundTracksFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
-
+        setupViewModel()
     }
 
     private fun setupRecyclerView() {
         binding.tracksRv.adapter = adapter
+    }
+
+    private fun setupViewModel() {
+        lifecycleScope.launch {
+            viewModel.tracksState.collectLatest {
+                when (it) {
+                    is Error -> TODO()
+                    is Progress -> TODO()
+                    is Result<*> -> TODO()
+                }
+            }
+        }
     }
 
     companion object {
