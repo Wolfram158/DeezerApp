5fcaab1bff16a5e7981d8637984e1d9010779357
Lubnin_Ilia <wolframalpha7134@gmail.com>
2025-04-20 22:08:53 +0300

    Remove unused function

--- DeezerApp/utils/src/main/java/android/learn/utils/Utils.kt
+++ DeezerApp/utils/src/main/java/android/learn/utils/Utils.kt
@@ -1,11 +1,3 @@
 package android.learn.utils
 
 typealias Seconds = Long
-
-@Suppress("DefaultLocale")
-fun formatTime(milliseconds: Long): String {
-    val totalSeconds = milliseconds / 1000
-    val minutes = totalSeconds / 60
-    val seconds = totalSeconds % 60
-    return String.format("%02d:%02d", minutes, seconds)
-}
